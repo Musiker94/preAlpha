100e0182472f00bd94b267640f791ff47ba8ef7a
KMS <kuzminykh_523@yahoo.com>
2016-10-12 02:08:20 +0700

    Добавлен класс Stream. Проверка формирования потока

--- AstroClass.py
+++ AstroClass.py
@@ -1,6 +1,7 @@
 from astropy.time import Time
 import numpy as np
 import datetime
+import random
 import math
 
 
@@ -150,17 +151,58 @@ def kepler2descart(elements, dim='deg'):
 
 
 class Body:
-    __semimajorAxis = None
-    __eccentricity = None
-    __inclination = None
-    __ascendingNode = None
-    __argumentPerihelion = None
-    __meanAnomaly = None
-    __trueAnomaly = None
-    __perihelionDistance = None
-    __coordinates = [None, None, None]
-    __velocities = [None, None, None]
-    __time_object = None
+
+    def __init__(self, a_q, e, i, om, w, v_m, param='axis', anomaly='true', dim='deg'):
+        """Initiates object and sets a shape of orbit and body's position on it
+
+        :param a_q: semi major axis or perihelion distance
+        :param e: eccentricity
+        :param i: inclination
+        :param om: ascending node
+        :param w: argument of perihelion
+        :param v_m: true or mean anomaly
+        :param param: sets what value is used as the first parameter:
+                'axis' - semi major axis (default),
+                'distance' - perihelion distance
+        :param anomaly: sets what anomaly is used:
+                'true' - true anomaly (default)
+                'mean' - mean anomaly
+        :param dim: sets the dimension of the angular values:
+                'deg' - degrees (default),
+                'rad' - radians
+        :return: None
+
+        """
+        self.__eccentricity = e
+        if dim == 'deg':
+            self.__inclination = math.radians(i)
+            self.__ascendingNode = math.radians(om)
+            self.__argumentPerihelion = math.radians(w)
+            v_m = math.radians(v_m)
+        elif dim == 'rad':
+            self.__inclination = i
+            self.__ascendingNode = om
+            self.__argumentPerihelion = w
+
+        if param == 'axis':
+            self.__semimajorAxis = a_q
+            self.__perihelionDistance = a_q * (1.0 - e)
+        elif param == 'distance':
+            self.__perihelionDistance = a_q
+            self.__semimajorAxis = a_q / (1.0 - e)
+
+        if anomaly == 'true':
+            self.__trueAnomaly = v_m
+            self.__meanAnomaly = true2mean_anomaly(self.__eccentricity, v_m)
+        elif anomaly == 'mean':
+            self.__meanAnomaly = v_m
+            self.__trueAnomaly = mean2true_anomaly(self.__eccentricity, v_m)
+
+        [self.__coordinates, self.__velocities] = kepler2descart([self.__semimajorAxis, self.__eccentricity,
+                                                                  self.__inclination, self.__ascendingNode,
+                                                                  self.__argumentPerihelion, self.__trueAnomaly],
+                                                                 dim='rad')
+        self.__time_object = None
 
     def set_orbit_shape(self, a_q, e, i, om, w, param='axis', dim='deg'):
         """Sets the shape of the orbit
@@ -197,7 +239,7 @@ class Body:
             self.__semimajorAxis = a_q / (1.0 - e)
 
     def set_anomaly(self, v_m, anomaly='true', dim='deg'):
-        """Sets the position of the body on the orbit on the anomaly
+        """Sets the position of the body on an orbit by the anomaly
 
         :param v_m: true or mean anomaly
         :param anomaly: sets what anomaly is used:
@@ -224,7 +266,7 @@ class Body:
                                                                  dim='rad')
 
     def set_position(self, x, y, z, vx, vy, vz):
-        """Sets the position of the body on the coordinates and velocities"""
+        """Sets the position of the body by coordinates and velocities"""
         self.__coordinates = [x, y, z]
         self.__velocities = [vx, vy, vz]
         [a, e, i, om, w, v] = descart2kepler([x, y, z], [vx, vy, vz])
@@ -312,3 +354,64 @@ class Body:
                       'isot': self.__time_object.isot[0],
                       'byear': self.__time_object.byear[0]}
         return dictionary[form]
+
+    def get_info(self):
+        info = self.get_orbit(anomaly='mean')
+        info.append(self.get_date('byear'))
+        info.append(self.get_date('jd'))
+        return np.array(info)
+
+
+class Stream:
+
+    def __init__(self, parent, num_of_particles):
+        self.__particles = []
+        orbit = parent.get_orbit()
+        rc = 1.0
+        dens = 1.9
+        mass = 7.6E-06
+        rpart = math.pow(0.75 * mass / (math.pi * dens), 1.0 / 3.0)  # radius = [cm]
+        q0 = orbit[0] * (1.0 - orbit[1])
+        p = orbit[0] * (1.0 - orbit[1] ** 2)
+        Q = orbit[0] * (1.0 + orbit[1])
+
+        for count in range(0, num_of_particles):
+            r = math.pow((1 / (random.random() * (math.pow(Q, -3.0) - math.pow(q0, -3.0))
+                               + math.pow(q0, -3.0))), 1.0 / 3.0)
+            v0 = (p - r) / (r * orbit[1])
+            v0 = math.acos(v0)
+            if random.random() < 0.5:
+                v0 = 2 * math.pi - v0
+            parent.set_anomaly(v0)
+            [x30, vel30] = parent.get_position()
+            cos_T = 1.0 - 2.0 * random.random()
+            sin_T = (1.0 - cos_T ** 2) ** 2
+            fi = 2.0 * math.pi * random.random()
+            c = math.sqrt(1 / (dens * rpart * math.pow(np.dot(x30, x30), 9.0 / 8.0))
+                          - 0.013 * rc) * math.sqrt(rc) * 656  # cm/sec
+            c = c * (6.68458712E-9 / 1.15740741)  # au/day
+            c3 = np.array([c * cos_T, c * sin_T * math.cos(fi), c * sin_T * math.sin(fi)])
+            mark = descart2kepler(x30, np.array(vel30) + c3)
+
+            particle = Body(mark[0], mark[1], mark[2], mark[3], mark[4], mark[5], dim='rad')
+            n = math.sqrt((MU / mark[0] ** 3))
+            mean_anomaly = particle.get_orbit(anomaly='mean', dim='rad')[5]
+            if mean_anomaly >= math.pi:
+                jd = parent.get_date('jd') - mean_anomaly / n
+            else:
+                jd = parent.get_date('jd') + mean_anomaly / n
+
+            particle.set_epoch(jd)
+
+            self.__particles.append(particle)
+
+    def __getitem__(self, item):
+        return self.__particles[item]
+
+    def get_info(self):
+        info = np.zeros([len(self.__particles), 9])
+        for count in range(0, len(self.__particles)):
+            info[count, 0:8] = self.__particles[count].get_info()
+            info[count, 8] = count + 1
+
+        return info
---
+++ main.py
@@ -0,0 +1,9 @@
+import AstroClass as AC
+import numpy as np
+
+
+test = AC.Body(1.192, 0.619, 70.8, 282.9, 171.4, 0.0, 'distance')
+test.set_epoch(2452700.5)
+stream = AC.Stream(test, 10000)
+
+np.savetxt('testFile.txt', stream.get_info(), fmt='%17.9f', delimiter='\t')
