982cf883cff6f1f0f833e3307485ed7063816cd5
KMS <kuzminykh_523@yahoo.com>
2016-10-14 02:56:03 +0700

    Залита информация о потоке из 100 частиц на 1000 лет

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import AstroClass as AC
 import numpy as np
 
 
-test = AC.Body(1.192, 0.619, 70.8, 282.9, 171.4, 0.0, 'distance')
-test.set_epoch(2452700.5)
-stream = AC.Stream(test, 10000)
+# test = AC.Body(1.192, 0.619, 70.8, 282.9, 171.4, 0.0, 'distance')
+# test.set_epoch(2452700.5)
+# stream = AC.Stream(test, 10000)
 
-np.savetxt('file.in', stream.get_info(), fmt='%17.9f', delimiter='\t')
+# np.savetxt('file.in', stream.get_info(), fmt='%17.9f', delimiter='\t')
+
