1a5af140c792a8f6d9bf713a06e83d2eccd74444
KMS <kuzminykh_523@yahoo.com>
2016-10-14 23:46:05 +0700

    Поправлены структуры файлов, добавлен файл с информацией о больших планетах (info_planets.csv)

--- main.py
+++ main.py
@@ -3,17 +3,23 @@ import pandas as pd
 import numpy as np
 
 
+def km2au(kilometers):
+    return kilometers / 149597870.700
+
+
 # test = AC.Body(1.192, 0.619, 70.8, 282.9, 171.4, 0.0, 'distance')
 # test.set_epoch(2452700.5)
 # stream = AC.Stream(test, 10000)
 
 # np.savetxt('file.in', stream.get_info(), fmt='%17.9f', delimiter='\t')
 
-# earth = np.loadtxt('DATA/Evolut/earth.out')
-# np.savetxt('earth.csv', earth, header='distance year jd planet node number', comments='')
+earth = np.loadtxt('DATA/Evolut/earth.out')
+np.savetxt('earth.csv', earth, header='distance year jd planet node number', comments='')
 
-# planets = np.loadtxt('DATA/Evolut/planets.out')
-# np.savetxt('planets.csv', planets, header='distance year jd planet number', comments='')
+planets = np.loadtxt('DATA/Evolut/planets.out')
+np.savetxt('planets.csv', planets, header='distance year jd planet number', comments='')
 
-# particles = np.loadtxt('DATA/Evolut/file.out')
-# np.savetxt('particles.csv', particles, header='a e i Om w M year jd number node', comments='')
+particles = np.loadtxt('DATA/Evolut/file.out')
+np.savetxt('particles.csv', particles, header='a e i Om w M year jd number node', comments='')
+
+
