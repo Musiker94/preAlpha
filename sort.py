1a573994c1fcea3c461220e4200e51baded284ae
KMS <kuzminykh_523@yahoo.com>
2016-10-18 20:38:29 +0700

    Отсортированы "погибшие" метеороиды

--- sort.py
+++ sort.py
@@ -2,7 +2,13 @@ import pandas as pd
 
 
 planets = pd.read_csv('planets.csv', ' ')
-now = planets[planets.number == 1]
-avg = sum(now.distance) / len(now)
-now = now[now.distance <= avg]
+unique = planets.number.unique()
+deaths = planets[planets.number == unique[0]]
+deaths = deaths[deaths.index == deaths.first_valid_index()]
 
+for count in unique[1:]:
+    now = planets[planets.number == count]
+    now = now[now.index == now.first_valid_index()]
+    deaths = deaths.append(now)
+
+deaths.to_csv('deaths.csv', index_label='ID')
